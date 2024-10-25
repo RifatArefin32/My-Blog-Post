4a33bc47a33bd2127d92110eab276a9510f9ace5
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-25 14:49:47 +0600

    Connect postgresql to the project

--- myblog_project/myblog_project/settings.py
+++ myblog_project/myblog_project/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'myblog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblog_project',
+        'USER': 'myblog_project_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  # Set to empty string for localhost
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
