2ec9e537ceab5c3088e18ee50ca0a0c0ca2e35cb
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-25 16:25:56 +0600

    Remove __pycache__ files from version control and update .gitignore

---
+++ myblog_project/mbp_authentication/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MbpAuthenticationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mbp_authentication'
--- myblog_project/myblog_project/urls.py
+++ myblog_project/myblog_project/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from mbp_authentication.views import RegisterView, LoginView, DashboardView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
+    path('api/auth/register', RegisterView.as_view(), name='auth_register'),
+    path('api/auth/login', LoginView.as_view(), name='auth_login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('api/dashboard/', DashboardView.as_view(), name='dashboard')
 ]
