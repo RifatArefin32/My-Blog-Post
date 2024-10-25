eb2fdece64c67e09c66defdf3bd71999d8d96621
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-25 18:46:07 +0600

    Update the serializer to show the auther information at the JSON data, make some changes at the view

--- myblog_project/mbp_blog_app/serializers.py
+++ myblog_project/mbp_blog_app/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import BlogPost
+from mbp_authentication.serializers import UserSerializer
 
 class BlogPostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
     class Meta:
         model = BlogPost
         fields = ['id', 'title', 'content', 'author', 'created_at']
