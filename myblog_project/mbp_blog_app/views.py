77ab97bef1948714707ce6c10901eb37790568e9
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-25 18:06:02 +0600

    Create views for blogpost

--- myblog_project/mbp_blog_app/views.py
+++ myblog_project/mbp_blog_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
-# Create your views here.
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
