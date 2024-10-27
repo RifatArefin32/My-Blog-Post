f02b70fcfde9411c5863162d6fa2f290d21a6e44
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-27 20:12:45 +0600

    Add Comment model

--- myblog_project/mbp_comments_app/models.py
+++ myblog_project/mbp_comments_app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+from mbp_blog_app.models import BlogPost
 
-# Create your models here.
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.blog_post.title}"
